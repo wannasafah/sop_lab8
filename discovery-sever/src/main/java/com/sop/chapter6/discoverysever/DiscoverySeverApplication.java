0a3d32927c0eeba8889dc055d656d5c6dd8e902d
fahfiily <63070156@kmitl.ac.th>
2022-10-11 16:19:38 +0700

    First commit

---
+++ discovery-sever/src/main/java/com/sop/chapter6/discoverysever/DiscoverySeverApplication.java
@@ -0,0 +1,15 @@
+package com.sop.chapter6.discoverysever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoverySeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverySeverApplication.class, args);
+	}
+
+}
